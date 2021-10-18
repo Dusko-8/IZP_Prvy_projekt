15c4523cc29e4db48637633aca784744b64a356c
Dušan <68845210+Dusko-8@users.noreply.github.com>
2021-10-18 14:56:01 +0200

    nacitanie vsetkych parametrov

--- main.c
+++ main.c
@@ -1,151 +1,90 @@
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
-#include <string.h>
 
-//funkcia ktora dostane z argv char a vrati int s overenim pre level
-int getIntLevel(char *argv){
-    char *endptr;
-    int i = strtoul(argv,&endptr,10);
+/*
+ *function which returns Level int to main
+ */
+int getIntLevel(char *argv) {
+  char *endptr;
+  int i = strtoul(argv, &endptr, 10);
 
-    if(endptr[0] != '\0'){
-      fprintf(stderr,"Parameter Level musi byt cele kladne cislo  \n");
-      return -1;
-    }
-    if(i>4 || i<1){
-      fprintf(stderr,"Parameter Level sa ma nachadzat v rozmedzi od 1 - 4  \n");
-
-      return -1;
-    }
-    
-    
-
-    return i;
+  // here program checks if the int we want passes our requirements
+  if (endptr[0] != '\0') {
+    fprintf(stderr, "Parameter Level musi byt cele kladne cislo  \n");
+    return -1;
   }
-//funkcia ktora dostane z argv char a vrati int s overenim pre param
-int getIntParam(char *argv){
-    char *endptr;
-    int i = strtoul(argv,&endptr,10);
+  if (i > 4 || i < 1) {
+    fprintf(stderr, "Parameter Level sa ma nachadzat v rozmedzi od 1 - 4  \n");
 
-    if(endptr[0] != '\0' || i<=0){
-      fprintf(stderr,"Parameter  musi byt cele kladne cislo  \n");
-      return -1;
-    }
-    
-
-    return i;
+    return -1;
   }
 
-char* getCharStats(char *argv){
-  char str1[] = "--stats";
-  char *str2 = {argv}; 
- 
-  //str2[1] = strdup(argv[3]);
-  //porovnajChar(str1,str2); 
-  
-  return str2;
+  return i;
+}
+/*
+ *function which returns Level int to main
+ */
+int getIntParam(char *argv) {
+  char *endptr;
+  int i = strtoul(argv, &endptr, 10);
+
+  if (endptr[0] != '\0' || i <= 0) {
+    fprintf(stderr, "Parameter  musi byt cele kladne cislo  \n");
+    return -1;
+  }
+
+  return i;
+}
+/*
+ *returns truo or fals based on if the 3rd parameter was exactly -stats
+ */
+bool getBoolStats(char *array1) {
+  int i;
+  char array2[] = "--stats";
+  bool response = false;
+  printf("--a--%s\n", &array1[0]);
+  i = 0;
+  // prejde kazdym prvkom char pola a porovna jednolive pozicie
+  while (array1[i] == array2[i] && response == 0) {
+    if (array1[i] == '\0' || array2[i] == '\0') {
+      response = true;
+    }
+    i++;
+  }
+
+  return response;
 }
 
-bool porovnajChar(char *str1, char *str2){
-  //int size1 = sizeof(str1) / sizeof(str1[0]); 
-  printf("str 1 %s\n",&str1[2]);
-  printf("str 2 %s\n",&str2[1]);
-  for (int i = 1;str1[i] == '\0' || str2[i] == '\0';i++)
-  {
-      if (str1[i] == str2[i]){
-        printf("str - %i -- %s\n",&i,&str1[i]);
-        printf("str - %i -- %s\n",&i,&str2[i]);
-        if (str1[i+1] == '\0' && str2[i+1] == '\0')
-        {
-          return true;
-        }else 
-        {
-          return false;
-        }
-        
-         i++;
-    }else{
-      return false;
-    }
-  }
-  
-  
-
-
-}
-
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   printf("Number of arguments: %d\n", argc);
- 
-  // inicializace promenne i 
- 
-  int i = 1;
-  // prvni a dalsi podminky 
-  /*while (argc > i)
-  {
-    // telo cyklu 
-    printf("%d. argument: %s\n", i, argv[i]);
-    // inkrementace 
-    i = i + 1;
-  }*/
-  //inicializacia premennych pre parametre
+
   int level;
   int param;
-  bool stats = false;
+  bool stats;
 
-  //nacitanie parametrov
-  if(argc > 2 && argc < 5){
+  // nacitanie parametrov
+  if (argc > 2 && argc < 5) {
+
     level = getIntLevel(argv[1]);
     param = getIntParam(argv[2]);
-    getCharStats(argv[3]);
-    char *str1 = "Ahogjh";
-    char *str2 = "Ahoj";
+    stats = getBoolStats(argv[3]);
 
-    if(str1[0] == str2[0]){
-      printf("-Rovnaju sa\n");
-
-    }else{
-      printf("Neovnajusa\n");
-    }
-    
-
-    printf("----------------------\n");
-    if(porovnajChar(str1,str2)==true){
-      printf("rovnaju sa\n");
-    }else
-    {
-      printf("nerovnaju\n");
-    }
-    
-
-    /*if (getCharParametre(argv[3]))
-    {
-      bool stats = true;
-    }*/
-    
-    
-
-    /*if(argc == 3){
-      stats = true;
-    }*/
-  }else{
+  } else {
     printf("Zly pocet parametrov \n");
   }
-  printf("level- %d param- %d stats- %d \n",level,param,stats);
+  
+  printf("level- %d param- %d stats- %d \n", level, param, stats);
+
   
   
-
-
-
-  char riadok[101];
-  while (fgets(riadok,101,stdin)!= NULL)
-  {
-    printf("%s",riadok);
-  }
   
   
+  
+  /*char riadok[101];
+  while (fgets(riadok, 101, stdin) != NULL) {
+    printf("%s", riadok);
+  }*/
 
- 
   return 0;
 }
